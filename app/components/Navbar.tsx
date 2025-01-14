2cf0300fe5803b0e01a9980e862ffedc8441e428
Shahzaib-Khan-200024 <codewithkhan946@gmail.com>
2025-01-14 06:15:42 +0500

    Fixed type issues and refined utilities for better compatibility

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
             <Link href="/" className="font-bold text-3xl">
-            KHAN'S<span className="text-primary"> BLOG</span>
+            KHANS<span className="text-primary"> BLOG</span>
             </Link>
 
             <ModeToggle />
